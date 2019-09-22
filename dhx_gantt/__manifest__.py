84b0ae9db21009d9908824654c4b6a8fcfe0fc4b
obi <obi@sudani.sd>
2019-09-22 23:00:42 +0200

    edit version

--- dhx_gantt/__manifest__.py
+++ dhx_gantt/__manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': [
