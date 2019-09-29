e45889d5f9eff79de7ad9bf7d08b1e4fe554763c
Obay Abdelgadir <obayit@gmail.com>
2019-09-29 13:57:47 +0200

    Update __manifest__.py

--- dhx_gantt/__manifest__.py
+++ dhx_gantt/__manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "Ubay Abdelgadir",
-    'website': "http://www.yourcompany.com",
+    'website': "https://github.com/obayit/odoo_dhtmlxgantt",
     'license': "GPL-2",
 
     # Categories can be used to filter modules in modules listing
