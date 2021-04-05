a014b8c14e7916c5bf9f096a536dcf0399421eae
obi <obayit@gmail.com>
2021-04-05 10:13:40 +0200

    [FIX] readd ondelete for selection_add

--- dhx_gantt/models/ir_act_window.py
+++ dhx_gantt/models/ir_act_window.py
@@ -4,5 +4,4 @@ from odoo import models, fields
 class IrActWindowView(models.Model):
     _inherit = 'ir.actions.act_window.view'
 
-    view_mode = fields.Selection(selection_add=[('dhx_gantt', "DHX Gantt")])
-    # , ondelete={'dhx_gantt': 'cascade'})  # v14.0
+    view_mode = fields.Selection(selection_add=[('dhx_gantt', "DHX Gantt")], ondelete={'dhx_gantt': 'cascade'})
--- dhx_gantt/models/ir_ui_view.py
+++ dhx_gantt/models/ir_ui_view.py
@@ -4,5 +4,4 @@ from odoo import models, fields
 class IrUiView(models.Model):
     _inherit = 'ir.ui.view'
 
-    type = fields.Selection(selection_add=[('dhx_gantt', "DHX Gantt")])
-    # , ondelete={'dhx_gantt': 'cascade'})  # v14.0
+    type = fields.Selection(selection_add=[('dhx_gantt', "DHX Gantt")], ondelete={'dhx_gantt': 'cascade'})
