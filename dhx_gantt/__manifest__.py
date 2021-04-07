b4f85f619ba56b4a634aa078c020a286f98f4f7f
Ubay Abdelgadir <obayit@gmail.com>
2021-04-07 18:08:18 +0200

    Fix version number

--- dhx_gantt/__manifest__.py
+++ dhx_gantt/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Project Management',
-    'version': '1.1',
+    'version': '14.1',
 
     # any module necessary for this one to work correctly
     'depends': [
